fddae7305fcfb31b7a6ec64abec7d76273d32723
Stefan Boskovic <bs140659d@student.etf.bg.ac.rs>
2022-08-28 21:49:39 +0200

    Adding assignees to add item component.

--- frontend/src/app/components/add-item/add-item.component.ts
+++ frontend/src/app/components/add-item/add-item.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { map, Observable, startWith } from 'rxjs';
 import { Currency } from 'src/app/models/currency';
 import { Project } from 'src/app/models/project';
+import { User } from 'src/app/models/user';
 import { ProjectsService } from 'src/app/services/projects.service';
 
 @Component({
@@ -17,6 +19,10 @@ export class AddItemComponent implements OnInit {
   currencies: Array<Currency> = [];
   currenciesMap: { [key: number]: number } = {};
   selectedCurrencyId = 1;
+
+  assigneeControl: FormControl = new FormControl();
+  departmentUsers: Array<User> = [];
+  filteredUsers: Observable<Array<User>> | undefined;
 
   constructor(private projectsService: ProjectsService, private _snackBar: MatSnackBar, private router: Router) {}
 
@@ -33,6 +39,13 @@ export class AddItemComponent implements OnInit {
         this.currenciesMap[currency.id] = currency.ratioToEur;
       }
     });
+    this.projectsService.getUsersInDepartment().subscribe((users) => {
+      this.departmentUsers = users;
+    });
+    this.filteredUsers = this.assigneeControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
   }
 
   submitItem(formData: any) {
@@ -52,6 +65,15 @@ export class AddItemComponent implements OnInit {
         this._snackBar.open(`Added: "${formData.name}".`, 'Dismiss');
         this.router.navigate(['/editProject']);
       });
+  }
+
+  private _filter(value: string): Array<User> {
+    const filterValue = this._normalizeValue(value);
+    return this.departmentUsers.filter((user) => this._normalizeValue(user.username).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
   }
 
   getCostInEur() {
